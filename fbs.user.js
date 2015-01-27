d5417f59bf77ebd23f4780926af4fe4c998da193
witiko <witiko@gmail.com>
2015-01-27 18:21:57 +0100

    Fix for (posted) not working, when the next message is only appended to the <li> element of the previous one.

--- fbs.user.js
+++ fbs.user.js
@@ -9,17 +9,17 @@
 // @require        https://www.dropbox.com/s/mgelvou7twxfngy/grammar.js?dl=1
 // @downloadURL    https://www.dropbox.com/s/5eet5uqk54xdwlc/fbs.user.js?dl=1
 // @grant          none
-// @version        1.04
+// @version        1.05
 // ==/UserScript==
-
+ 
 /*
-
+ 
   Input: <Message1/Command1><Message2/Command2> ... <MessageN/CommandN>(;)
          <Message1/Command1><Message2/Command2> ... <MessageN/CommandN>(;)
          <Message1/Command1><Message2/Command2> ... <MessageN/CommandN>
-         
+          
          Each of the sections delimited by (;) is processed in parallel to the others.
-
+ 
   Commands:
     Responding to actions:
           (seen) - Wait until the previous message has been marked as seen
@@ -28,7 +28,7 @@
         (typing) - Wait until the recipient has started typing to you
        (typing!) - Wait until the recipient has started typing to you / posted a message
            (any) - Wait until the recipient has started typing to you / posted a message / seen the previous message
-
+ 
     Responding to states:
         (online) - Wait until the recipient has gone online
        (!online) - Wait until the recipient is no longer online
@@ -36,7 +36,7 @@
        (!mobile) - Wait until the recipient is no longer mobile
        (offline) - Wait until the recipient has gone offline
       (!offline) - Wait until the recipient is no longer offline
-      
+       
   User-defined events:
         (^EVENT)
      or (^EVENT^)  - Emit an event named EVENT in the current window passing undefined as the message          (using custom events)
@@ -46,7 +46,7 @@
  (^^EVENT^^DATA^^) - Emit an event named EVENT in all open windows passing String(eval("DATA")) as the message (using custom events and localStorage events)
         (:EVENT)   - Wait until the event named EVENT has occured in the current window and capture it  (edge-triggered -- waits until the event occurs)
        (::EVENT)   - Wait until the event named EVENT has occured in the current window and capture it (level-triggered -- returns immediately, if the event has already been captured)
-
+ 
     Miscellaneous:
              (v) - Redirect all following messages to console.log
              (^) - Redirect all following messages to the current recipient (implicit)
@@ -57,7 +57,7 @@
                  - If isNaN(parseHMS("#1")), then an exception is logged and (at #1) evaluates to (never)
    (/...) (//...
     or (//...//) - These commands are ignored (comments)
-
+ 
    (<#1><unit1> <#2><unit2> ...
     <#N><unitN>) - Wait \sum_{1\leq I\leq N}#i <unitI>, where <unitI>\in:
                Y - Years
@@ -67,27 +67,27 @@
                m - Minutes
                s - Seconds
               ms - Milliseconds
-   
+    
     (js)...(js)
     or (js)...   - Execute the enclosed javascript code
           `...`  - Execute the enclosed javascript code and substitute the command for its return value. (weak)
                    Weak substitution is only allowed within messages.
                    Weak substitution is non-recursive -- its return value is always regarded as a message.
-                   
-                   Examples:                   
+                    
+                   Examples:                  
                      Did you know that 1 + 2 = `1 + 2`?  ~>  Did you know that 1 + 2 = 3?
                      Hey, `getCurrName()`, I am `getMyName()`.  ~>  Hey, Karel, I am Vít.
-                   
+                    
          ``...`` - Execute the enclosed javascript code and substitute the command for its return value. (strong)
                    Strong substitution is allowed anywhere.
                    Strong substitution is recursive -- its return value is always retokenized.
-                   
+                    
                    Examples:
                      (``1 + 2``s)You've got five seconds to tell me where I am and three have just passed.
                      This will get posted.(``condition ? "^" : "v"``)And this will conditionally not get posted.
-                   
+                    
                    The following additional methods are available for execution, substitution and event sending:
-
+ 
      getCurrName() - The whole name of the recipient
        getMyName() - The first name of the sender
          eventData - The data of the last captured event
@@ -96,27 +96,27 @@
           editRc() - Pastes the entire fbsrc into the message box.
                      Double-clicking the message box saves the new fbsrc.
     log(arg1, ...) - Log the arguments into the console.
-
+ 
   Name locking:
     Each input you execute is locked to the name of the current recipient.
     Input execution will be paused each time you switch to another user.
     Fbsrc inputs are exempt from this rule.
-
+ 
   Fbsrc:
     You can store a sequence of inputs delimited by (cr) in localStorage.fbsrc.
     These inputs will be automatically executed (without name locking) each time the userscript is loaded.
-  
+   
   Tokenization:
     The input tokenization is performed in three steps (see function tokenize):
-    
+     
       1) In the first step, the input is split into sections delimited by (cr). For each section:
         a) In the first step, the section is split into comments and non-comments and comments are discarded.
         b) In the second step non-comments are split into strong substitution / commands and messages.
         c) In the third step, adjoining strong substitution and messages are concatenated into compound messages.
-    
+     
   Execution:
     The input string is tokenized and sequentially processed (see function execute):
-    
+     
       1) If the token is a command, the command is performed.
       2) If the token is a message, then:
         a) If the message contains a strong substitution, the substitution is performed, the resulting string
@@ -124,10 +124,10 @@
         b) Otherwise:
            i) If the message contains a weak substitution, the substitution is performed.
           ii) The resulting string is sent to the current recipient / logged.
-        
-
+         
+ 
 */
-
+ 
 (function() {
   if(window.top != window && window.top != window.unsafeWindow) return;
   var rawCommands = /\(js\)(?:.*?)(?:\(js\)|$)|\(v\)|\(\^\)|\(\^\^[a-zA-Z0-9\-.]+?\^\^.+?\^\^\)|\(\^\^[a-zA-Z0-9\-.]+?(?:\^\^)?\)|\(\^[a-zA-Z0-9\-.]+?\^.+?\^\)|\(\^[a-zA-Z0-9\-.]+?(?:\^)?\)|\(::?[a-zA-Z0-9\-.]+?\)|\(at .*?\)|\((?:\d+(?:Y|M|d|h|ms|m|s)\s?)+\)|\(never\)|\(seen\)|\(typing!?\)|\(any\)|\(beep\)|\(replied\)|\(posted\)|\(!?(?:(?:on|off)line|mobile)\)/,
@@ -182,9 +182,9 @@
       REPLY_SELECTOR = "div[role=log] li.webMessengerMessageGroup",
       MY_NAME_SELECTOR = "._2dpb", qualifiedName = "name.witiko.fbs.",
       pastEvents = {};
-  
+   
   log("fbs running");
-
+ 
   (function() {
     // Executing fbsrc
     onload = function() {
@@ -192,13 +192,13 @@
         parseAndExecute(localStorage["fbsrc"]);
       } log("fbsrc executed");
     };
-
+ 
     // Building a GUI
     var input = document.createElement("div"), highlighter,
         DEFAULT_TEXT = "(v)",
         ERASABLE = /^(\(v\)|\s*|<br\s*\/?\s*>)$/i,
         hidden = true;
-    
+     
     if(!localStorage.curr) {
       localStorage.curr = localStorage.max = 0;
       localStorage[0] = DEFAULT_TEXT;
@@ -264,7 +264,7 @@
     }; document.body.appendChild(input);
     document.body.appendChild(highlighter);
     highlight();
-
+ 
     function placeCaretAtEnd(el) {
       el.focus();
       if (typeof window.getSelection != "undefined"
@@ -282,7 +282,7 @@
         textRange.select();
       }
     }
-    
+     
     function parseAndExecute(string, name) {
       string.split(rawCr).forEach(function(input) {
         var batch = tokenize(input);
@@ -294,7 +294,7 @@
         })();
       });
     }
-    
+     
     function highlight() {
       highlighter.innerHTML = input.innerHTML.split(cr).map(function(text) {
         return cr.test(text) ? "<span style=\"background-color: " + colors.cr + "\">" + text + "</span>"
@@ -307,23 +307,23 @@
                   match  = events.globalSend.data.test(text) ? text.match(events.globalSend.data) : text.match(events.localSend.data);
               coloredText = "<span style=\"background-color: " + colors.commands + "\">(" + spikes + match[1] + spikes + "</span>" +
                       "<span style=\"background-color: " + colors.substitution.weak + "\">" + match[2] + "</span>" +
-                      "<span style=\"background-color: " + colors.commands + "\">" + spikes + ")";
+                      "<span style=\"background-color: " + colors.commands + "\">" + spikes + ")</span>";
             } else {
               var color = substitution.strong[1].test(text) ? colors.substitution.strong
                                                             : colors[evals.test(text) ? "js" : "commands"];
               coloredText = "<span style=\"background-color: " + color + "\">" + text + "</span>";
             }
             return tokens.test(text) ? coloredText : text.split(substitution.weak[0]).map(function(text) {
-              return substitution.weak[0].test(text) ? 
+              return substitution.weak[0].test(text) ?
                 "<span style=\"background-color: " + colors.substitution.weak + "\">" + text + "</span>" : text;
             }).join("");
           }).join("");
         }).join("");
       }).join("");
     }
-    
+     
   })();
-
+ 
   function beep() { // Let us let out a beep
   var osc = ctx.createOscillator();
       osc.type = 3;
@@ -332,22 +332,22 @@
     setTimeout(function() {
       osc.noteOff(0);
       osc.disconnect();
-    }, 500); 
+    }, 500);
   }
-
+ 
   function parseTime(time) {
     var curr = 0;
     takeTwo(time.replace(/\s/g, "").split(units), function(fst, snd) {
       curr += +fst * unitValues[snd];
     }); return curr;
-
+ 
     function takeTwo(arr, callback) {
       for(var i = 0, l = Math.floor(arr.length / 2); i !== l; ++i)
         callback(arr[i * 2],
                  arr[i * 2 + 1]);
     }
   }
-  
+   
   function parseHMS(str) {
     // A well-formedness guard
     if(!hms.test(str)) return NaN;
@@ -372,11 +372,11 @@
       return date.getTime() + unitValues.d;
     } return date.getTime();
   }
-  
+   
   function tokenize(string) {
     // Remove comments and tokenize the input into commands, messages and strong substitutions
     var batch = string.replace(comments, "").split(tokens).filter( function(s) { return s.trim(); });
-    
+     
     // Aggregate adjoining messages and strong substitutions
     return mapTwo(batch, function(a, b) {
       if(!commands.test(a) && substitution.strong[0].test(b) ||
@@ -386,7 +386,7 @@
         return [ a,  b ];
       }
     });
-
+ 
     function mapTwo(arr, f) {
       var result = arr.slice(0);
       for (var i = 1; i < result.length; ) {
@@ -397,95 +397,95 @@
       } return result;
     }
   }
-  
+   
   function now() {
     return Date.now();
   }
-  
+   
   function send(message) {
     if(!message) return;
     setMessage(message);
     document.querySelector("input[value=\"Odpovědět\"]").click();
   }
-
+ 
   function whisper(message) {
     if(!message) return;
     log(message);
   }
-  
+   
   function getMessage() {
     var el = document.querySelector(MESSAGE_SELECTOR);
     return el.value;
   }
-  
+   
   function setMessage(string) {
     var el = document.querySelector(MESSAGE_SELECTOR);
     el.classList.remove(PLACEHOLDER_CLASS);
     el.value = string;
   }
-  
+   
   function clearMessage() {
     var el = document.querySelector(MESSAGE_SELECTOR);
     el.classList.add(PLACEHOLDER_CLASS);
     el.value = "";
   }
-
+ 
   function execute(batch, silent, name, context, eventData) {
     if(!batch.length) return;
     if(commands.test(batch[0]) &&
       !substitution.strong[0].test(batch[0])) { // It's a command
-      var command = batch[0].replace(/\((.*?)\).*/, "$1");  
+      var command = batch[0].replace(/\((.*?)\).*/, "$1"); 
       var prefix, suffix, namelocked = false,
           currReplyId = getLastReplyId();
       if(command) {
         prefix = command.replace(/\s.*/, "");
         suffix = command.replace(/.*?\s/, "");
       }
-      
+       
       switch(prefix) { // Let's handle it
-        
+         
         case "seen":    waitUntil(seen);    break;
         case "replied": waitUntil(replied); break;
         case "typing":  waitUntil(typing);  break;
-
+ 
         case "posted": waitUntil(function() {
           return replied() && changed();
         }); break;
-
+ 
         case "typing!": waitUntil(function() {
           return typing() || (replied() && changed());
         }); break;
-
+ 
         case "any": waitUntil(function() {
           return typing() || seen() || (replied() && changed());
         }); break;
-
+ 
         case "online": waitUntil(function() {
           return document.querySelector(".presenceActive");
         }); break;
-
+ 
         case "!online": waitUntil(function() {
           return !document.querySelector(".presenceActive");
         }); break;
-
+ 
         case "mobile": waitUntil(function() {
           return document.querySelector(".presenceMobile");
         }); break;
-
+ 
         case "!mobile": waitUntil(function() {
           return !document.querySelector(".presenceMobile");
         }); break;
-
+ 
         case "offline": waitUntil(function() {
           return !document.querySelector(".presenceActive") &&
                  !document.querySelector(".presenceMobile");
         }); break;
-
+ 
         case "!offline": waitUntil(function() {
           return document.querySelector(".presenceActive") ||
                  document.querySelector(".presenceMobile");
         }); break;
-
+ 
         case "^": silent = false; next(); break;
         case "v": silent = true;  next(); break;
         case "beep": perform(beep); break;
@@ -495,7 +495,7 @@
           if (isNaN(at)) {
             log(suffix, "specifies a malformed date.");
           } else setTimeout(perform, at - now()); break;
-          
+           
         case "js": perform(function() {
           with(context) {
             try {
@@ -505,32 +505,32 @@
             }
           }
         }); break;
-
+ 
         default:
           // An outgoing global event with data
           if(events.globalSend.data.test(batch[0])) (function(matches) {
             globalSend(matches[1], evaluate(matches[2]));
             next();
           })( batch[0].match(events.globalSend.data) );
-          
+           
           // An outgoing global event without data
           else if(events.globalSend.nodata.test(batch[0])) (function(matches) {
             globalSend(matches[1], "undefined");
             next();
           })( batch[0].match(events.globalSend.nodata) );
-            
+             
           // An outgoing local event with data
           if(events.localSend.data.test(batch[0])) (function(matches) {
             localSend(matches[1], evaluate(matches[2]));
             next();
           })( batch[0].match(events.localSend.data) );
-          
+           
           // An outgoing local event without data
           else if(events.localSend.nodata.test(batch[0])) (function(matches) {
             localSend(matches[1], "undefined");
             next();
           })( batch[0].match(events.localSend.nodata) );
-            
+             
           // A level-triggered incoming event
           else if(events.receive.level.test(batch[0])) (function(matches) {
             if(matches[1] in pastEvents) {
@@ -538,11 +538,11 @@
               next();
             } else waitFor(matches[1]);
           })( batch[0].match(events.receive.level) );
-            
+             
           // An edge-triggered incoming event
           else if(events.receive.edge.test(batch[0]))
             waitFor(batch[0].match(events.receive.edge)[1]);
-          
+           
           // A timeout command
           else setTimeout(perform, parseTime(command));
       }
@@ -556,22 +556,23 @@
         (silent?whisper:send)(substitute(batch[0], "weak").trim());
       }
     });
-    
+     
     function replied() {
       return document.querySelector("div[role=log] li:last-child strong > a").href !== document.querySelector("a._2dpe").href;
     } function changed() {
-      return getLastReplyId() !== currReplyId();
+      return getLastReplyId() !== currReplyId;
     } function typing() {
       return !!document.querySelector(".typing");
     } function seen() {
       return document.querySelector("._kv .seenByListener").classList.contains("seenByAll");
     }
-
+ 
     function getLastReplyId() {
-      var messages = document.querySelectorAll(REPLY_SELECTOR);
-      return messages[messages.length - 1].id;
+      var messages = document.querySelectorAll(REPLY_SELECTOR),
+          el = messages[messages.length - 1];
+      return el.id + "#" + el.querySelectorAll("p").length;
     }
-
+ 
     function waitUntil(condition) {
       doWhen(condition, function() {});
     } function perform(action) {
@@ -623,12 +624,12 @@
         }, data: data
       });
     } function waitFor(name) {
-      
+       
       // Global listener
       var obj = {}, done = false;
       obj[qualifiedName + name] = callback;
       ding.listen(obj);
-      
+       
       // Local listener
       var node = new JBus.Node({
         group: qualifiedName + name
@@ -638,7 +639,7 @@
           callback(msg.data.payload);
         }
       });
-      
+       
       function callback(data) {
         if(done) return;
         done = true;
@@ -647,22 +648,22 @@
       }
     }
   }
-  
+   
   // User utility functions to be used inside of substitutions
   function getLastReply() {
     var messages = document.querySelectorAll(REPLY_SELECTOR);
     var paragraphs = messages[messages.length - 1].querySelectorAll("p");
     return paragraphs[paragraphs.length - 1].textContent;
   }
-  
+   
   function getCurrName() {
     return webMessengerHeaderName.getElementsByTagName("a")[0].textContent;
   }
-  
+   
   function getMyName() {
     return document.querySelector(MY_NAME_SELECTOR).textContent;
   }
-  
+   
   function editRc() {
     log("editing fbsrc");
     setMessage(localStorage.fbsrc);
@@ -679,9 +680,9 @@
       document.querySelector(MESSAGE_SELECTOR).ondblclick = null;
     }
   }
-  
+   
   function log() {
     console.log.apply(console, [new Date].concat([].slice.call(arguments, 0)));
   }
-  
+   
 })();
